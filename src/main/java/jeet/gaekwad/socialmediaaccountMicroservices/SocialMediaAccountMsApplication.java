e943cf01eb976a12c768d92069ac1b2b31eadc5e
jeet gaekwad <gaekwadjeet22@gmail.com>
2024-10-21 08:51:32 +0530

    My first commit

---
+++ src/main/java/jeet/gaekwad/socialmediaaccountMicroservices/SocialMediaAccountMsApplication.java
@@ -0,0 +1,13 @@
+package jeet.gaekwad.socialmediaaccountMicroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialMediaAccountMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocialMediaAccountMsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/jeet/gaekwad/socialmediaaccountMicroservices/SocialMediaAccountMsApplicationTests.java
@@ -0,0 +1,13 @@
+package jeet.gaekwad.socialmediaaccountMicroservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMediaAccountMsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
